4958ef5f786b31286cdf56d09276f7a1133e6a6e
Indrek Lasn <lasnindrek@gmail.com>
2017-07-17 17:29:23 +0300

    there's video? cool

--- src/app/components/AstronomyCard.js
+++ src/app/components/AstronomyCard.js
@@ -2,16 +2,47 @@ import React from 'react';
 
 const AstronomyCard = (props) => {
 
-	const { title, url, hdurl, explanation, date, copyright } = props.data;
+	const { title,
+	 url,
+	 hdurl,
+	 explanation,
+	 date,
+	 copyright,
+	 media_type
+	} = props.data;
+
+	function renderContent() {
+		switch(media_type) {
+
+			case('video'):
+				return (
+					<iframe
+						allowFullScreen
+	    			frameBorder="0"
+	    			height="520"
+	    			width="720"
+	    			src={url}>
+    			</iframe>
+				)
+
+				case('image'):
+					return (
+						<a href={hdurl} className="astronomy-image-wrapper">
+								<img src={url} alt={title} />
+						</a>
+		    	)
+
+		    	default:
+		    		return null
+		}
+	}
 
 	return (
 		<div className="astronomy-card">
 
 			<h6 className="astronomy-title">{title}</h6>
 
-			<a href={hdurl} className="astronomy-image-wrapper">
-					<img src={url} alt={title} />
-			</a>
+			{renderContent()}
 
 			<p>{explanation}</p>
 
